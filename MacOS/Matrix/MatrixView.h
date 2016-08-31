aa516a8135b82a1c6501815270ce3fb88621f20d
Andy.Li <Andy.Li@movit-tech.com>
2016-08-31 20:30:52 +0800

    init osx prj

---
+++ MacOS/Matrix/MatrixView.h
@@ -0,0 +1,13 @@
+//
+//  MatrixView.h
+//  Matrix
+//
+//  Created by Andy.Lee on 16/8/31.
+//  Copyright © 2016年 Andy.Lee. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface MatrixView : ScreenSaverView
+
+@end
