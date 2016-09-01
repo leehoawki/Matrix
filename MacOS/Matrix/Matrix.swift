e8525a7a6906606c998399a72fc95f332108fa37
leehoawki <leehoawki@gmail.com>
2016-09-01 20:38:27 +0800

    update

--- MacOS/Matrix/Matrix.swift
+++ MacOS/Matrix/Matrix.swift
@@ -3,6 +3,27 @@ import ScreenSaver
 
 class MatrixView: ScreenSaverView {
     
+    let width = 128
+    let height = 72
+    let flips = 5
+    let garbage = "1234567890!@#$%^&*()ZXCVBNM<>?"
+    
+    var count = 0
+    var switches = [Int](count:width, repeatedValue:1)
+    
+    let attributes = [
+        NSForegroundColorAttributeName:NSColor.greenColor()
+    ]
+    
+    override init?(frame: NSRect, isPreview: Bool) {
+        super.init(frame: frame, isPreview: isPreview)
+        super.animationTimeInterval = 1/30.0
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
     override func startAnimation() {
         super.startAnimation()
     }
@@ -16,7 +37,19 @@ class MatrixView: ScreenSaverView {
     }
     
     override func animateOneFrame() {
+        for i in 0 ... flips {
+            let x = arc4random_uniform(UInt32(width))
+            if (switches[x] == 0) {
+                switches[x] = 1
+            } else {
+                switches[x] = 0
+            }
+        }
         
+        
+        
+        let t = "test"
+        t.drawInRect(NSMakeRect(100, 100, 100,100), withAttributes: attributes)
     }
     
     override func hasConfigureSheet() -> Bool {
